bb27f19c721f2c6d5a733f89ac99c6e6582fb820
Ineb01 <benjamin.bachmayr@gmail.com>
2024-09-08 23:16:53 +0200

    modularize

--- main.tf
+++ main.tf
@@ -10,56 +10,18 @@ module "proxy" {
   source = "./modules/proxy"
 }
 
-resource "tls_private_key" "master" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+module "proxmox-node-1" {
+  source = "./modules/ubuntu-nodes/proxmox"
+  ip = "192.168.1.32"
+  vm-name = "k3s-home-01"
+  proxmox_node = var.proxmox_node
+  id = 100
 }
 
-resource "proxmox_vm_qemu" "srv_demo_1" {
-  name = "k3s-01"
-  desc = "Ubuntu Server for k3s"
-  target_node = var.proxmox_node
-  sshkeys = tls_private_key.master.public_key_openssh
-  agent = 1
-  clone = "ubuntu-cloud"
-  qemu_os = "l26"
-  cores = 2
-  cpu = "host"
-  memory = 4096
-  scsihw = "virtio-scsi-pci"
-
-  serial {
-    id = "0"
-    type = "socket"
-  }
-
-  disks {
-    ide {
-      ide2 {
-        cloudinit {
-          storage = "local-lvm"
-        }
-      }
-    }
-    scsi {
-      scsi0 {
-        disk {
-          discard              = false
-          emulatessd           = true
-          format               = "raw"
-          size                 = "30G"
-          storage              = "local-lvm"
-        }
-      }
-    }
-  }
-
-  network {
-    bridge = "vmbr0"
-    model = "virtio"
-  }
-
-  os_type = "cloud-init"
-  ciuser = "ubuntu"
-  ipconfig0 = "ip=192.168.1.97/24,gw=192.168.1.1"
+module "proxmox-node-2" {
+  source = "./modules/ubuntu-nodes/proxmox"
+  ip = "192.168.1.33"
+  vm-name = "k3s-home-02"
+  proxmox_node = var.proxmox_node
+  id = 101
 }
