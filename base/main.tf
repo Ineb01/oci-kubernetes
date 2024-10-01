90a5cf9d3a148aed5251e2d68e011c42f57326ca
Benjamin Bachmayr <benjamin.bachmayr@gmail.com>
2024-10-01 11:37:58 +0200

    1 node setup for oci

--- base/main.tf
+++ base/main.tf
@@ -1,6 +1,7 @@
 module "argocd-deployment" {
   source = "../modules/argocd"
   domain = var.argocd_domain
+  depends_on = [ module.ingress, module.authentik-deployment ]
 }
 
 module "ingress" {
@@ -9,29 +10,22 @@ module "ingress" {
   private_key = var.private_key
   router_domain = var.router_domain
   proxmox_domain = var.proxmox_domain
+
 }
 
 module "authentik-deployment" {
   source = "../modules/authentik"
   domain = var.authentik_domain
   authentik_admin_token = var.authentik_admin_token
+  depends_on = [ module.ingress ]
 }
 
-output "akadmin_pw" {
+output "authentik_admin_pw" {
   value = module.authentik-deployment.akadmin_pw
   sensitive = true
 }
 
-module "proxmox_realm" {
-  source = "../modules/proxmox-realm"
-  domain = var.proxmox_domain
-}
-
 data "authentik_brand" "authentik-default" {
   domain = "authentik-default"
-}
-
-output "proxmox_command" {
-  value = module.proxmox_realm.proxmox_command
-  sensitive = true
+  depends_on = [ module.authentik-deployment ]
 }
--- cluster/main.tf
+++ cluster/main.tf
@@ -1,110 +1,147 @@
+
 resource "random_password" "k3s_token" {
   length           = 16
   special          = true
   override_special = "!#$%"
 }
 
-module "proxmox-node-1" {
-  source = "../modules/ubuntu-nodes/proxmox"
-  ip = "192.168.1.32"
-  vm-name = "k3s-home-01"
-  proxmox_node = var.proxmox_node
-  id = 100
+locals {
+  kubeconfig = yamldecode(replace(ssh_resource.kubeconfig.result, "127.0.0.1", module.instance.public_ip[0]))
 }
 
-resource "ssh_resource" "install_k3s_1" {
+resource "tls_private_key" "ssh_tls" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-  host         = module.proxmox-node-1.ip
-  user         = module.proxmox-node-1.user
-  
-  private_key = module.proxmox-node-1.tls_private_key
+resource "oci_core_vcn" "vnc" {
+  compartment_id = var.tenancy_ocid
+  cidr_block     = "172.19.0.0/16"
+  display_name   = "kubernetes_vnc"
+}
+
+resource "oci_core_internet_gateway" "igw" {
+  compartment_id = var.tenancy_ocid
+  vcn_id         = oci_core_vcn.vnc.id
+  display_name   = "kubernetes_igw"
+}
+
+resource "oci_core_route_table" "rtb" {
+  compartment_id = var.tenancy_ocid
+  vcn_id         = oci_core_vcn.vnc.id
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.igw.id
+    destination       = "0.0.0.0/0"
+  }
+}
+
+resource "oci_core_route_table_attachment" "attachment" {
+  subnet_id = oci_core_subnet.this.id
+  route_table_id = oci_core_route_table.rtb.id
+}
+
+resource "oci_core_security_list" "security_list" {
+  compartment_id = var.tenancy_ocid
+  vcn_id         = oci_core_vcn.vnc.id
+  display_name   = "all-traffic"
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
+  ingress_security_rules {
+    protocol = "all"
+    source   = "0.0.0.0/0"
+  }
+}
+
+resource "oci_core_subnet" "this" {
+  cidr_block        = "172.19.1.0/24"
+  compartment_id    = var.tenancy_ocid
+  vcn_id            = oci_core_vcn.vnc.id
+  security_list_ids = [oci_core_security_list.security_list.id]
+}
+
+data "oci_core_images" "oracle_linux" {
+  compartment_id = var.tenancy_ocid
+  operating_system = "Oracle Linux"
+  shape = "VM.Standard.A1.Flex"
+  operating_system_version = "7.9"
+  sort_by = "TIMECREATED"
+  sort_order = "DESC"
+}
+
+module "instance" {
+  source                     = "oracle-terraform-modules/compute-instance/oci"
+  compartment_ocid           = var.tenancy_ocid
+  instance_display_name      = "kubernetes_node"
+  source_ocid                = data.oci_core_images.oracle_linux.images[0].id
+  subnet_ocids               = [oci_core_subnet.this.id]
+  public_ip                  = "EPHEMERAL"
+  ssh_public_keys            = tls_private_key.ssh_tls.public_key_openssh
+  block_storage_sizes_in_gbs = [50]
+  shape                      = "VM.Standard.A1.Flex"
+}
+
+resource "ssh_resource" "open_firewall" {
+
+  host = module.instance.public_ip[0]
+  user = "opc"
+
+  private_key = tls_private_key.ssh_tls.private_key_openssh
 
   timeout = "15m"
 
   commands = [
-    "curl -sfL https://get.k3s.io | K3S_TOKEN=${random_password.k3s_token.result} sh -s - server --cluster-init --disable traefik --write-kubeconfig-mode 644 --node-name k3s-home-01"
+    "sudo firewall-cmd --zone=public --add-port=6443/tcp --permanent",
+    "sudo firewall-cmd --reload"
   ]
+}
 
-  depends_on = [ module.proxmox-node-1 ]
+resource "ssh_resource" "install_k3s_1" {
+
+  host = module.instance.public_ip[0]
+  user = "opc"
+
+  private_key = tls_private_key.ssh_tls.private_key_openssh
+
+  timeout = "15m"
+
+  commands = [
+    "curl -sfL https://get.k3s.io | K3S_TOKEN=${random_password.k3s_token.result} sh -s - server --cluster-init --disable traefik --write-kubeconfig-mode 644 --node-name k3s-home-01 --tls-san ${module.instance.public_ip[0]}"
+  ]
 }
 
 resource "ssh_resource" "kubeconfig" {
 
-  host         = module.proxmox-node-1.ip
-  user         = module.proxmox-node-1.user
-  
-  private_key = module.proxmox-node-1.tls_private_key
+  host = module.instance.public_ip[0]
+  user = "opc"
+
+  private_key = tls_private_key.ssh_tls.private_key_openssh
 
   timeout = "1m"
 
   commands = [
-     "cat /etc/rancher/k3s/k3s.yaml"
+    "cat /etc/rancher/k3s/k3s.yaml"
   ]
-  depends_on = [ ssh_resource.install_k3s_1 ]
+  depends_on = [ssh_resource.install_k3s_1]
 }
+
 
 resource "local_file" "kubeconfig" {
   filename = "/home/ineb01/.kube/config"
   content  = yamlencode(local.kubeconfig)
 }
 
-resource "local_file" "node1-tls" {
-  filename = "./.terraform/node-1"
-  content  = module.proxmox-node-1.tls_private_key
+resource "local_file" "ssh_key" {
+  content         = tls_private_key.ssh_tls.private_key_pem
+  file_permission = "600"
+  filename        = "/home/ineb01/.ssh/nodes"
 }
 
-module "proxmox-node-2" {
-  source = "../modules/ubuntu-nodes/proxmox"
-  ip = "192.168.1.33"
-  vm-name = "k3s-home-02"
-  proxmox_node = var.proxmox_node
-  id = 101
-}
-
-resource "ssh_resource" "install_k3s_2" {
-
-  host         = module.proxmox-node-2.ip
-  user         = module.proxmox-node-2.user
-  
-  private_key = module.proxmox-node-2.tls_private_key
-
-  timeout = "15m"
-
-  commands = [
-    "curl -sfL https://get.k3s.io | K3S_TOKEN=${random_password.k3s_token.result} sh -s - server --server https://192.168.1.32:6443 --disable traefik --node-name k3s-home-02"
-  ]
-  depends_on = [ ssh_resource.install_k3s_1 ]
-}
-
-module "proxmox-node-3" {
-  source = "../modules/ubuntu-nodes/proxmox"
-  ip = "192.168.1.34"
-  vm-name = "k3s-home-03"
-  proxmox_node = var.proxmox_node
-  id = 102
-}
-
-resource "ssh_resource" "install_k3s_3" {
-
-  host         = module.proxmox-node-3.ip
-  user         = module.proxmox-node-3.user
-  
-  private_key = module.proxmox-node-3.tls_private_key
-
-  timeout = "15m"
-
-  commands = [
-    "curl -sfL https://get.k3s.io | K3S_TOKEN=${random_password.k3s_token.result} sh -s - server --server https://192.168.1.32:6443 --disable traefik --node-name k3s-home-03"
-  ]
-  depends_on = [ ssh_resource.install_k3s_1 ]
-}
-
-variable "proxmox_node" {
-  type = string
-}
-
-locals {
-  kubeconfig = yamldecode(replace(ssh_resource.kubeconfig.result, "127.0.0.1", module.proxmox-node-1.ip))
+resource "local_file" "ssh_key_pub" {
+  content         = tls_private_key.ssh_tls.public_key_openssh
+  file_permission = "755"
+  filename        = "/home/ineb01/.ssh/nodes.pub"
 }
 
 output "kubernetes_host" {
