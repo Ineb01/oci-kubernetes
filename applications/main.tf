94dbb9e1948804ef9109e18f73d7b6b2dfd06354
Benjamin Bachmayr <benjamin.bachmayr@gmail.com>
2024-10-11 00:04:01 +0200

    vsc server init

--- applications/main.tf
+++ applications/main.tf
@@ -34,3 +34,37 @@ module "reverse-proxy-hello-static" {
   external-domain = "hellostatic.cluster.dphx.eu"
 }
 
+resource "kubernetes_manifest" "vsc-server" {
+  manifest = yamldecode(<<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: vscode-server
+  namespace: argocd
+spec:
+  destination:
+    namespace: applications
+    server: https://kubernetes.default.svc
+  project: default
+  source:
+    chart: code-server
+    repoURL: https://nicholaswilde.github.io/helm-charts/
+    targetRevision: 1.1.1
+  syncPolicy:
+    automated: {}
+    syncOptions:
+    - PrunePropagationPolicy=foreground
+    - CreateNamespace=true
+EOF
+)
+}
+
+module "reverse-proxy-vsc" {
+  source = "../modules/auth-proxy"
+  svc-name = "vscode-server"
+  svc-namespace = "applications"
+  svc-port = 8443
+  display_name = "VSCode"
+  name = "vsc"
+  external-domain = "vsc.cluster.dphx.eu"
+}
