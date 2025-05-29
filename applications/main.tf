6beb464f77c0e58ab279066a7db24ea6ec39a787
Benjamin Bachmayr <benjamin.bachmayr@gmail.com>
2025-05-29 15:19:38 +0200

    change to application repo

--- applications/main.tf
+++ applications/main.tf
@@ -4,88 +4,22 @@ resource "kubernetes_namespace" "applications" {
   }
 }
 
-
-resource "kubernetes_manifest" "hello-static" {
+resource "kubernetes_manifest" "applications" {
   manifest = yamldecode(<<EOF
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata:
-  name: 'hello-static'
+  name: applications
   namespace: argocd
 spec:
   project: default
   source:
-    repoURL: https://github.com/Ineb01/k8s-static-web
+    repoURL: https://github.com/Ineb01/argo-applications.git
     path: .
     targetRevision: HEAD
   destination:
     server: https://kubernetes.default.svc
-    namespace: applications
-  syncPolicy:
-    automated:
-      prune: true
-      selfHeal: true
-    syncOptions:
-      - Replace=true
-EOF
-)
-  depends_on = [ kubernetes_namespace.applications ]
-}
-
-module "reverse-proxy-hello-static" {
-  source = "../modules/auth-proxy"
-  svc-name = "nginx-svc"
-  svc-namespace = "hello-static"
-  svc-port = 80
-  display_name = "Hello Static"
-  name = "hello-static"
-  external-domain = "hellostatic.cluster.dphx.eu"
-  outpost_kubernetes_integration_id = var.outpost_kubernetes_integration_id
-}
-
-resource "kubernetes_manifest" "open-webui" {
-  manifest = yamldecode(<<EOF
-apiVersion: argoproj.io/v1alpha1
-kind: Application
-metadata:
-  name: open-webui
-  namespace: argocd
-spec:
-  project: default
-  source:
-    repoURL: https://github.com/Ineb01/open-webui.git
-    path: .
-    targetRevision: HEAD
-  destination:
-    server: https://kubernetes.default.svc
-    namespace: applications
-  syncPolicy:
-    automated:
-      prune: true
-      selfHeal: true
-    syncOptions:
-      - Replace=true
-EOF
-)
-  depends_on = [kubernetes_namespace.applications]
-}
-
-resource "kubernetes_manifest" "vsc-server" {
-  manifest = yamldecode(<<EOF
-apiVersion: argoproj.io/v1alpha1
-kind: Application
-metadata:
-  name: vscode-server
-  namespace: argocd
-spec:
-  project: default
-  source:
-    repoURL: https://github.com/Ineb01/vsc-server-setup.git
-    path: .
-    targetRevision: HEAD
-  destination:
-    server: https://kubernetes.default.svc
-    namespace: applications
+    namespace: argocd
   syncPolicy:
     automated:
       prune: true
@@ -105,5 +39,17 @@ module "reverse-proxy-vsc" {
   display_name = "VSCode"
   name = "vsc"
   external-domain = "vsc.cluster.dphx.eu"
+  outpost_kubernetes_integration_id = var.outpost_kubernetes_integration_id
+}
+
+
+module "reverse-proxy-hello-static" {
+  source = "../modules/auth-proxy"
+  svc-name = "nginx-svc"
+  svc-namespace = "hello-static"
+  svc-port = 80
+  display_name = "Hello Static"
+  name = "hello-static"
+  external-domain = "hellostatic.cluster.dphx.eu"
   outpost_kubernetes_integration_id = var.outpost_kubernetes_integration_id
 }
