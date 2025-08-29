be3cc25f06a096174dda0095cc9cb0015be1eb77
Benjamin Bachmayr <b.bachmayr@auvaria.com>
2025-08-29 11:41:32 +0200

    removed legacy stuff

--- applications/main.tf
+++ applications/main.tf
@@ -30,26 +30,3 @@ EOF
 )
   depends_on = [kubernetes_namespace.applications]
 }
-
-module "reverse-proxy-vsc" {
-  source = "../modules/auth-proxy"
-  svc-name = "vscode-server"
-  svc-namespace = kubernetes_namespace.applications.metadata[0].name
-  svc-port = 8443
-  display_name = "VSCode"
-  name = "vsc"
-  external-domain = "vsc.cluster.dphx.eu"
-  outpost_kubernetes_integration_id = var.outpost_kubernetes_integration_id
-}
-
-
-module "reverse-proxy-hello-static" {
-  source = "../modules/auth-proxy"
-  svc-name = "nginx-svc"
-  svc-namespace = "hello-static"
-  svc-port = 80
-  display_name = "Hello Static"
-  name = "hello-static"
-  external-domain = "hellostatic.cluster.dphx.eu"
-  outpost_kubernetes_integration_id = var.outpost_kubernetes_integration_id
-}
